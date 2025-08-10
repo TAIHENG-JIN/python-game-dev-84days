88b797cf644b31ab056726f6a7803246df86eb5c
TAIHENG-JIN <2227572569@qq.com>
2025-08-10 22:30:02 +0900

    Add files via upload

--- week01/day01_hello.py
+++ week01/day01_hello.py
@@ -1,3 +1,3 @@
 print("This is a 'Hello, World!' program in Python.")
-print("I'm about to do a long term python game development practice project in 84 days.")
-print("Whis me good luck!")
+print("I'm about to do a long term python game development project in 84 days.")
+print("Wish me good luck!")
